e88666bfbc28745e60335241925d6a6393f599d1
HenriqueRib <ribeiro.henriquem@gmail.com>
2020-01-17 17:12:41 -0300

    Atualizado

--- Maior_ou_menor/teste.rb
+++ Maior_ou_menor/teste.rb
@@ -1,31 +1,31 @@
 class Application
   # To change this template use File | Settings | File Templates.
   def initialize
-    mainMenu
+  	mainMenu
   end
 
   def navigateTo(what)
-    what.new.display
-    mainMenu
+  	what.new.display
+  	mainMenu
   end
 
   def mainMenu
-    puts "O que voçê quer fazer?
-      1: Jogar
-      2: Configurações
-      3: Sair"
-case gets.strip
-  when "1"
-    Jogar
-  when "2"
-    Configurações
-  when "3"
-    system "exit"
-  defaut:
-	puts"Opcao invalida"
-	break;	
+  	puts "O que voçê quer fazer?
+  	1: Jogar
+  	2: Configurações
+  	3: Sair"
+  	case gets.strip
+  	when "1"
+  		Jogar
+  	when "2"
+  		Configurações
+  	when "3"
+  		system "exit"
+  		defaut:
+  		puts"Opcao invalida"
+  		break;	
 
-end
-end
-Application.new
+  	end
+  end
+  Application.new
 end
